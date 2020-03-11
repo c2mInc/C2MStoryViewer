63393fd1a4c482b63ed07aa7a29fa7cb09e8ca63
Cuneyt <cuneyt.carikci@gmail.com>
2020-03-11 16:17:26 +0300

    change story sample urls

--- app/src/main/java/com/c2m/storyviewer/StoryGenerator.kt
+++ app/src/main/java/com/c2m/storyviewer/StoryGenerator.kt
@@ -6,35 +6,67 @@ object StoryGenerator {
 
     fun generateStories(): ArrayList<StoryUser> {
         val storyUrls = ArrayList<String>()
-        storyUrls.add("https://images.wallpaperscraft.com/image/railway_train_station_134586_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/road_marking_bridge_123398_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/bridge_autumn_city_121892_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/ruins_man_loneliness_124279_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/night_city_signs_neon_139551_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/tokyo_night_city_skyscrapers_121628_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/new_zealand_panorama_skyscrapers_buildings_shore_lighting_119195_1080x1920.jpg")
-        storyUrls.add("https://images.wallpaperscraft.com/image/city_evening_snowfall_136478_1080x1920.jpg")
+        storyUrls.add("https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1236701/pexels-photo-1236701.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1535162/pexels-photo-1535162.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1433052/pexels-photo-1433052.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1366630/pexels-photo-1366630.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1067333/pexels-photo-1067333.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1122868/pexels-photo-1122868.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1837603/pexels-photo-1837603.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1612461/pexels-photo-1612461.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1591382/pexels-photo-1591382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/53757/pexels-photo-53757.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
+        storyUrls.add("https://images.pexels.com/photos/134020/pexels-photo-134020.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1367067/pexels-photo-1367067.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1420226/pexels-photo-1420226.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
+        storyUrls.add("https://images.pexels.com/photos/2217365/pexels-photo-2217365.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/2260800/pexels-photo-2260800.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/1719344/pexels-photo-1719344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/364096/pexels-photo-364096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/3849168/pexels-photo-3849168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/2953901/pexels-photo-2953901.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/3538558/pexels-photo-3538558.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+        storyUrls.add("https://images.pexels.com/photos/2458400/pexels-photo-2458400.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+
 
         val userProfileUrls = ArrayList<String>()
-        userProfileUrls.add("https://randomuser.me/api/portraits/women/10.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/women/1.jpg")
         userProfileUrls.add("https://randomuser.me/api/portraits/men/1.jpg")
-        userProfileUrls.add("https://randomuser.me/api/portraits/women/7.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/women/2.jpg")
         userProfileUrls.add("https://randomuser.me/api/portraits/men/2.jpg")
         userProfileUrls.add("https://randomuser.me/api/portraits/women/3.jpg")
-        userProfileUrls.add("https://randomuser.me/api/portraits/men/11.jpg")
-        userProfileUrls.add("https://randomuser.me/api/portraits/women/14.jpg")
-        userProfileUrls.add("https://randomuser.me/api/portraits/men/21.jpg")
-        userProfileUrls.add("https://randomuser.me/api/portraits/women/28.jpg")
-        userProfileUrls.add("https://randomuser.me/api/portraits/men/16.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/men/3.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/women/4.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/men/4.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/women/5.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/men/5.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/women/6.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/men/6.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/women/7.jpg")
+        userProfileUrls.add("https://randomuser.me/api/portraits/men/7.jpg")
 
         val storyUserList = ArrayList<StoryUser>()
-        for (i in 1..10){
+        for (i in 1..10) {
             val stories = ArrayList<Story>()
-            val storySize = Random.nextInt(1,5)
-            for (j in 0 until storySize){
-                stories.add(Story(storyUrls[Random.nextInt(storyUrls.size)], System.currentTimeMillis() - (1 * (24-j) * 60 * 60 * 1000)))
+            val storySize = Random.nextInt(1, 5)
+            for (j in 0 until storySize) {
+                stories.add(
+                    Story(
+                        storyUrls[Random.nextInt(storyUrls.size)],
+                        System.currentTimeMillis() - (1 * (24 - j) * 60 * 60 * 1000)
+                    )
+                )
             }
-            storyUserList.add(StoryUser("username$i",userProfileUrls[Random.nextInt(userProfileUrls.size)],stories))
+            storyUserList.add(
+                StoryUser(
+                    "username$i",
+                    userProfileUrls[Random.nextInt(userProfileUrls.size)],
+                    stories
+                )
+            )
         }
 
         return storyUserList
